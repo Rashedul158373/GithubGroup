bc07ba2042aa4416def0d3109e44912d1e5f4197
NABDroid <nabdroid313@gmail.com>
2020-01-07 16:34:55 +0600

    Update from Masliia

---
+++ app/src/main/java/com/example/githubgroup/MasliniaActivity.java
@@ -0,0 +1,14 @@
+package com.example.githubgroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MasliniaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maslinia);
+    }
+}
